8c41c78b2ff01de23fe85db1f7bf77821bbd49c3
Richard G <cod3ninj4@ninjanode>
2012-10-05 16:15:54 -0400

    Adds Euler problem 6

---
+++ 6.go
@@ -0,0 +1,26 @@
+package main
+// The difference between the sum of the squares of the first 100 natural numbers and the square of the sum.
+
+import (
+	"fmt"
+)
+
+func square_sum(x int) int{
+	var sum int = 0
+	for i := 1; i <= x; i++ {
+		sum = sum + i
+	}
+	return sum * sum
+}
+
+func square_natural(x int) int{
+	var total int = 0
+	for i := 1; i <=x; i++ {
+		total = total + ( i * i )
+	}
+	return total
+}
+
+func main() {
+	fmt.Printf("Difference: %d\n",square_sum(100) - square_natural(10))
+}
