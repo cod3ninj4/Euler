7c37e5c9b98c584b25bc528e056c697d5aa75b7c
Richard G <cod3ninj4@ninjanode>
2012-10-05 12:06:23 -0400

    Adds Euler problem 5

---
+++ 5.go
@@ -0,0 +1,41 @@
+package main
+// Smallest positive number that is evenly divisible by number 1 - 20
+
+import (
+	"fmt"
+)
+
+func is_divisible(x, y int) bool {
+	var result bool = false
+	if x % y == 0 {
+		result = true
+	}
+	return result
+}
+
+func divis_1_to_20(x int) bool {
+	var divis bool = true
+	for divis == true{
+		for i := 2; i <= 20; i+= 1{
+			divis = is_divisible(x, i)
+			if divis == false{
+				i = 99
+			}
+		}
+		return divis
+	}
+	return divis
+}
+
+func main(){
+	var value int = 2518
+	var divisible bool = false
+	for divisible != true {
+		value += 2
+		divisible = divis_1_to_20(value)
+	}
+	
+	fmt.Printf("Smallest value is: %d\n", value)
+	
+}
+	
