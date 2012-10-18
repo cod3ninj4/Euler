8432155f5aced2130abaf5378391a81f71f50ba4
Richard G <cod3ninj4@ninjanode>
2012-10-18 14:26:33 -0500

    Optimizes Euler 12, now runs in 1.135s

--- 12.go
+++ 12.go
@@ -1,22 +1,24 @@
-package main
-
+package main 
+/*
+  First triagle number with over 500 factors.
+  Note, you only need to check up to the SQRT of the number b/c every
+  factor below that has a mirror, hence factors + 2
+*/
 import (
 	"fmt"
+	"math"
 )
-
 func main() {
 	number := 0
 	for x := 1; x > 0 ; x++ {
-		number += x
-		var factors int = 0
-		if number % 2 == 0 {
-			for i := number; i > 0; i-- {
-				if number % i == 0 {
-					factors++
-				}
+	number += x
+	factors := 0
+		for i := 1; i <= int(math.Sqrt(float64(number))); i++ {
+			if number % i == 0 {
+				factors += 2
 			}
 		}
-		if factors >= 500 {
+		if factors > 500 {
 			fmt.Printf("Number: %d Factors: %d\n", number, factors)
 			goto Done
 		}
