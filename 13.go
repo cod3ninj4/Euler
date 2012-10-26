18ea1a6eed4e3a5540d919948df751846ce9dac4
Richard G <cod3ninj4@ninjanode>
2012-10-26 17:30:15 -0500

    Adds euler 13 and the text file for it

---
+++ 13.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	total := big.NewInt(0)
+	holder:= big.NewInt(0)
+	content, _ := ioutil.ReadFile("13.txt")
+	lines := strings.Split(string(content), "\n")
+	for x := 0 ; len(lines) > x+1; x++ {
+		holder, _ = holder.SetString(lines[x], 10)
+		total.Add(total, holder)
+	}
+	fmt.Println("Total: ", total.String()[:10])
+}
