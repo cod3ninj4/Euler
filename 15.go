f25a8723924a4ac757ae6df42d0f8ab98d8f63b2
Richard G <cod3ninj4@ninjanode>
2012-10-29 14:00:25 -0500

    Adds Euler problem 15

---
+++ 15.go
@@ -0,0 +1,23 @@
+package main
+// Find the number of unique down/right routes through a 20x20 grid
+import (
+	"fmt"
+	"math/big"
+)
+
+func factorial(n int64) *big.Int{
+	if n <= 0 {
+		return big.NewInt(1)
+	}
+	ans := big.NewInt(0)
+	return ans.Mul(big.NewInt(n), factorial(n-1))
+}
+func findRoutes(x int64, y int64) *big.Int{
+	routes := big.NewInt(0)
+	routes = routes.Mul(factorial(x), factorial(y))
+	return routes.Div(factorial(x+y), routes)
+}
+func main() {
+	routes := findRoutes(20, 20)
+	fmt.Printf("Routes: %v\n", routes)
+}
